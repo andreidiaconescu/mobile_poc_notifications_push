6131641f1c7920db9f39d0e53d07a1dbabb1490c
andrei.diaconescu <andrei.diaconescu@anais.digital>
2016-09-13 17:49:35 +0300

    ....

--- listenAndStoreGCMAppInstanceRegistrationTokens.php
+++ listenAndStoreGCMAppInstanceRegistrationTokens.php
@@ -18,3 +18,28 @@
     var_dump($_REQUEST);
 ?>
 </pre>
+
+<?php
+$tokenData = [];
+if (isset($_GET) && is_array($_GET)) {
+    $tokenData = array_merge($tokenData, $_GET);
+}
+if (isset($_POST) && is_array($_GET)) {
+    $tokenData = array_merge($tokenData, $_POST);
+}
+
+$inputStreamParams = file_get_contents('php://input');
+if ($inputStreamParams)
+$inputStreamParams = json_decode($inputStreamParams,true);
+if ($inputStreamParams && is_array($inputStreamParams)) {
+    $tokenData = array_merge($tokenData, $inputStreamParams);
+}
+?>
+
+<br>$tokenData:
+<pre>
+<?php
+    var_dump($tokenData);
+?>
+</pre>
+
